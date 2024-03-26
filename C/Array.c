e985dd28776ff81c31c394db0ff44a2dcb5357a3
Mi Taseen <114951279+InferiorAK@users.noreply.github.com>
2024-03-26 15:21:19 +0600

    Update Array.c

--- C/Array.c
+++ C/Array.c
@@ -1,17 +1,17 @@
 // Author: InferiorAK
 // Array in C Programming
 
-
 // string  --> char ArrayName[total_strings][string_length] = {element1, element2, ...}
 // integer --> int ArrayName[row][column] = {{num, num, num, ...}, {num, num, num, ...}, ...}
 
-
 #include <stdio.h>
 
-int chr_array(){
+int chr_array()
+{
 	char array[][4] = {"hey", "love", "you"};
 	int i;
-	for (i = 0; i <= 2; i++){
+	for (i = 0; i <= 2; i++)
+	{
 		printf("%s\n", array[i]);
 	}
 	printf("array simple demo: %s\n", array[0]);
@@ -23,22 +23,31 @@ int int_array()
 	printf("array2 simple demo: %d\n", array2[2]);
 
 	int array3[3][4] = {
-	// col1 col2 col3 col4
-		{1, 2, 3, 4},       // row1
-		{11, 12, 13, 14},   // row2
-		{21, 22, 23, 24}    // row3
+		// col1 col2 col3 col4
+		{1, 2, 3, 4},	  // row1
+		{11, 12, 13, 14}, // row2
+		{21, 22, 23, 24}  // row3
 	};
-	printf("array3 simple demo: %d\n", array3[1][3]);    // but everything starts with 0, here array3[1][3] means - array[row2][col4] element
+	printf("array3 simple demo: %d\n", array3[1][3]); // but everything starts with 0, here array3[1][3] means - array[row2][col4] element
 	int j, k;
-	for (j = 0; j <= 2; j++){
-		for (k = 0; k <= 3; k++){
+	for (j = 0; j <= 2; j++)
+	{
+		for (k = 0; k <= 3; k++)
+		{
 			printf("%d ", array3[j][k]);
 		};
 	};
+	printf("\n");
 }
 
-int main(){
+
+#define len(array) (sizeof(array) / sizeof(array[0]))
+int main()
+{
 	chr_array();
 	int_array();
+
+	int arr[] = {1, 2, 3, 4, 5};
+	printf("length of the array 'arr' is: %d\n", len(arr));
 	return 0;
 }
