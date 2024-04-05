f6c06da86579d83e1cf115ac9f2753b55a3ce887
Mi Taseen <114951279+InferiorAK@users.noreply.github.com>
2024-04-05 16:29:10 +0600

    Update Pointer.c

--- C/Pointer.c
+++ C/Pointer.c
@@ -11,13 +11,26 @@
 int main()
 {
     int x = 69;
-    int *y = &x;
+    int *y;
+    y = &x;
 
-    printf("%u\n", x);    // We will get the value of var x
-    printf("%u\n", &x);   // We will get the address of var x
-    printf("%u\n", y);   // We will get the adress of var x
-    printf("%u\n", *y);   // We will get the value of var x
+    printf("%u\n", x);  // We will get the value of var x
+    printf("%u\n", &x); // We will get the address of var x
+    printf("%u\n", y);  // We will get the adress of var x
+    printf("%u\n", *y); // We will get the value of var x
     // -> let me assume like this, * and & are opposite to each other, so *y=&x => y=x,
-    // ultimately it will print the value of var x
-    printf("%u\n", &y);   // We will get the adress of var y
+    // ultimately it will print the value of var x if I print *y
+    printf("%u\n\n", &y); // We will get the adress of var y
+
+    float a = 69.69;
+    void *ptr; // When we use void pointer then we don't get any trouble of declaring int, float, char.
+    // Like suppose- It will automatically detect the type casting!
+    ptr = &a;
+
+    printf("%f\n", a);
+    printf("%u\n", &a);
+    printf("%u\n", ptr);
+    printf("%f\n", *(float *)ptr); // *ptr -> *(type *)ptr .
+                                   // In void case, We have to declare the type there if we want to get the value of var a!
+    printf("%u\n", &ptr);
 }
