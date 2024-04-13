07e65a85d743515481d85f6c147587508bb8194e
Mi Taseen <114951279+InferiorAK@users.noreply.github.com>
2024-04-13 12:03:12 +0600

    Update Loops_Infinite.c

--- C/Loops_Infinite.c
+++ C/Loops_Infinite.c
@@ -5,23 +5,38 @@
 
 int while_loop()
 {
+    int i = 1;
     while (1) // while(any non-zero number)
     {
-        printf("While Loop!\n");
+        printf("While Loop %d!\n", i);
+        i++;
     }
+}
+
+int do_loop()
+{
+    int i = 1;
+    do
+    {
+        printf("Do Loop %d!\n", i);
+        i++;
+    } while (1);
 }
 
 int for_loop()
 {
+    int i = 1;
     for (;;)
     {
-        printf("For Loop!\n");
+        printf("For Loop %d!\n", i);
+        i++;
     }
 }
 
 int main()
 {
-    while_loop();
+    // while_loop();
+    do_loop();
     // for_loop();
     return 0;
 }
